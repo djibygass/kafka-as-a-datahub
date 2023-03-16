dedb68c59f9d4e68af290895e305222658d1c329
Jonathan Raffre <jonathan@raff.re>
2023-03-16 22:05:46 +0100

    chore: support java version & run configs

--- kafka-stream/src/main/scala/org/esgi/project/streaming/StreamProcessing.scala
+++ kafka-stream/src/main/scala/org/esgi/project/streaming/StreamProcessing.scala
@@ -1,18 +1,14 @@
 package org.esgi.project.streaming
 
-import io.github.azhur.kafkaserdeplayjson.PlayJsonSupport
-import org.apache.kafka.streams.KafkaStreams
-import org.apache.kafka.streams.kstream.Windowed
+import io.github.azhur.kafka.serde.PlayJsonSupport
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.streams.scala._
-import org.apache.kafka.streams.scala.kstream._
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
 
-import java.io.InputStream
 import java.util.Properties
 
 object StreamProcessing extends PlayJsonSupport {
-
-  import org.apache.kafka.streams.scala.ImplicitConversions._
-  import org.apache.kafka.streams.scala.serialization.Serdes._
 
   val applicationName = s"some-application-name"
 
@@ -36,15 +32,14 @@ object StreamProcessing extends PlayJsonSupport {
 
   // auto loader from properties file in project
   def buildProperties: Properties = {
-    import org.apache.kafka.clients.consumer.ConsumerConfig
-    import org.apache.kafka.streams.StreamsConfig
-    val inputStream: InputStream = getClass.getClassLoader.getResourceAsStream("kafka.properties")
-
     val properties = new Properties()
+    properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+    properties.put(StreamsConfig.CLIENT_ID_CONFIG, applicationName)
     properties.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationName)
+    properties.put(StreamsConfig.STATESTORE_CACHE_MAX_BYTES_CONFIG, "0")
     properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
     properties.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, "-1")
-    properties.load(inputStream)
+    properties.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1")
     properties
   }
 }
