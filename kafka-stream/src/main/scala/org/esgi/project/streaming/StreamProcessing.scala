4bc0a10ac8e15d4ad0627dd2eacdd73b03015d9a
Collas Pierre <collaspierre51@gmail.com>
2024-07-18 15:15:30 +0200

    test 1

--- kafka-stream/src/main/scala/org/esgi/project/streaming/StreamProcessing.scala
+++ kafka-stream/src/main/scala/org/esgi/project/streaming/StreamProcessing.scala
@@ -3,11 +3,13 @@ package org.esgi.project.streaming
 import io.github.azhur.kafka.serde.PlayJsonSupport
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.streams.kstream.{TimeWindows, Windowed}
 import org.apache.kafka.streams.scala._
 import org.apache.kafka.streams.scala.kstream.{KStream, KTable, Materialized}
 import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
 import org.esgi.project.streaming.models._
 
+import java.time.Duration
 import java.util.Properties
 
 object StreamProcessing extends PlayJsonSupport {
@@ -17,19 +19,27 @@ object StreamProcessing extends PlayJsonSupport {
 
   val applicationName = s"trade-statistics-app"
 
-  private val props: Properties = buildProperties
+  private val props: Properties = buildProperties()
 
   // defining processing graph
   val builder: StreamsBuilder = new StreamsBuilder
 
   val tradeTopic = "trades"
   val tradeCountStoreName = "trade-count-store"
+  val tradeCountPerMinuteStoreName = "trade-count-per-minute-store"
 
   val trades: KStream[String, Trade] = builder.stream[String, Trade](tradeTopic)
 
+  // Count trades per symbol
   val tradeCounts: KTable[String, Long] = trades
     .groupBy((_, trade) => trade.s)
     .count()(Materialized.as(tradeCountStoreName))
+
+  // Count trades per symbol per minute
+  val tradeCountsPerMinute: KTable[Windowed[String], Long] = trades
+    .groupBy((_, trade) => trade.s)
+    .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(1)).advanceBy(Duration.ofMinutes(1)))
+    .count()(Materialized.as(tradeCountPerMinuteStoreName))
 
   def run(): KafkaStreams = {
     val streams: KafkaStreams = new KafkaStreams(builder.build(), props)
@@ -45,11 +55,11 @@ object StreamProcessing extends PlayJsonSupport {
   }
 
   // auto loader from properties file in project
-  def buildProperties: Properties = {
+  def buildProperties(appName: Option[String] = None): Properties = {
     val properties = new Properties()
     properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
     properties.put(StreamsConfig.CLIENT_ID_CONFIG, applicationName)
-    properties.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationName)
+    properties.put(StreamsConfig.APPLICATION_ID_CONFIG, appName.getOrElse(applicationName))
     properties.put(StreamsConfig.STATESTORE_CACHE_MAX_BYTES_CONFIG, "0")
     properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
     properties.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, "-1")
