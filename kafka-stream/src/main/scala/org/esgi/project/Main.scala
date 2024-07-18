cf9449d4f3b5ed3ce80d894b8d719674dbb8dd57
bkyVGC <barqueroadrian111@gmail.com>
2024-07-18 16:44:02 +0200

    Update Websocket Connection

--- kafka-stream/src/main/scala/org/esgi/project/Main.scala
+++ kafka-stream/src/main/scala/org/esgi/project/Main.scala
@@ -1,14 +1,17 @@
-package org.esgi.project
-
-
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 import com.typesafe.config.{Config, ConfigFactory}
-import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.{KafkaStreams, StreamsBuilder}
+import org.apache.kafka.streams.scala.Serdes._
+import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.streams.scala.kstream._
 import org.esgi.project.api.WebServer
-import org.esgi.project.streaming.StreamProcessing
 import org.slf4j.{Logger, LoggerFactory}
 
+import java.util.Properties
 import scala.concurrent.ExecutionContextExecutor
 
 object Main {
@@ -18,11 +21,37 @@ object Main {
   val logger: Logger = LoggerFactory.getLogger(this.getClass)
   val config: Config = ConfigFactory.load()
 
-  def main(args: Array[String]) {
-    val streams: KafkaStreams = StreamProcessing.run()
+  def main(args: Array[String]): Unit = {
+    // Kafka Streams configuration
+    val props = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("application.id", "my-streams-app")
+    props.put("default.key.serde", Serdes.String().getClass.getName)
+    props.put("default.value.serde", Serdes.String().getClass.getName)
+
+    val builder = new StreamsBuilder()
+    val inputTopic = "trades"
+
+    val stream: KStream[String, String] = builder.stream[String, String](inputTopic)
+
+    stream.foreach((key, value) => {
+      println(s"Received record: key = $key, value = $value")
+    })
+
+    val topology = builder.build()
+    val streams = new KafkaStreams(topology, props)
+
+    streams.start()
+
+    startServer(streams)
+
+    logger.info(s"App started")
+  }
+
+  def startServer(streams: KafkaStreams): Unit = {
     Http()
       .newServerAt("0.0.0.0", 8080)
       .bindFlow(WebServer.routes(streams))
-    logger.info(s"App started on 8080")
+    logger.info(s"Server started on port 8080")
   }
 }
