37ced01fda83ba05c6ebe9eac5d65a677065bfbc
bkyVGC <barqueroadrian111@gmail.com>
2024-07-18 16:50:25 +0200

    Update API

--- kafka-stream/src/main/scala/org/esgi/project/api/WebServer.scala
+++ kafka-stream/src/main/scala/org/esgi/project/api/WebServer.scala
@@ -4,7 +4,16 @@ import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import de.heikoseeberger.akkahttpplayjson.PlayJsonSupport
 import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore
 import org.esgi.project.api.models.{MeanLatencyForURLResponse, VisitCountResponse}
+import org.esgi.project.streaming.models.MeanLatencyForURL
+import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.slf4j.LoggerFactory
+import play.api.libs.json.Json
+
+import java.util.Properties
+import scala.collection.JavaConverters._
 
 object WebServer extends PlayJsonSupport {
   def routes(streams: KafkaStreams): Route = {
@@ -21,6 +30,34 @@ object WebServer extends PlayJsonSupport {
           complete(
             List(MeanLatencyForURLResponse("", 0))
           )
+        }
+      },
+      path("trades") {
+        get {
+          complete {
+            val consumerProps = new Properties()
+            consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
+            consumerProps.put(ConsumerConfig.GROUP_ID_CONFIG, "web-server-group")
+            consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
+            consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getName)
+
+            val consumer = new KafkaConsumer[String, String](consumerProps)
+            consumer.subscribe(java.util.Collections.singletonList("trades"))
+
+            try {
+              val records = consumer.poll(java.time.Duration.ofSeconds(10)).asScala.toList
+              val jsonData = records.map { record =>
+                Json.obj(
+                  "key" -> record.key(),
+                  "value" -> record.value()
+                )
+              }
+
+              Json.toJson(jsonData)
+            } finally {
+              consumer.close()
+            }
+          }
         }
       }
     )
